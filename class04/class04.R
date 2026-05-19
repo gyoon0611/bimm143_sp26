66ff1ea6536fa3d425efd89c61b3bad0b84b384f
gyoon0611 <jul144@ucsd.edu>
2026-05-19 12:43:36 -0700

    Add class 4-10

---
+++ class04/class04.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Class 4: Intro to R"
+#' author: "Julianne Lee (PID: A17979401)"
+#' date: "Apr 9th, 2026"
+#' ---
+
+# This is just a text file
+x <- 1:50
+plot(x,sin(x),type="l", col="blue",lwd="3")
